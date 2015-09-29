c4dc463981a4d34963a1375a0cb93cea935d7197
alexballera <alexballera@gmail.com>
2015-09-28 23:13:05 -0430

    Terminada vistas y estilos de Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :show, :update, :destroy]  
+  before_action :authenticate_user!
+  before_action :set_user, except: [:index]  
 
   def index
     @users = User.all
