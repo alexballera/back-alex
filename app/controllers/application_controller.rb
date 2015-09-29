6a73d407a1db3356ace484b2b2735b952b7a2ed0
alexballera <alexballera@gmail.com>
2015-09-29 09:15:48 -0430

    Configurado permisos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,10 @@ def set_locale
   I18n.locale = params[:locale] || I18n.default_locale
 end
 
+def default_url_options(options={})
+  { locale: I18n.locale }
+end
+
 protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,22 +14,34 @@ class UsersController < ApplicationController
 
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, :notice => 'El usuario fue editado exitosamente' }
-        format.json { render :show, status: :ok, location: @user }
+      if current_user.is_admin?
+        if @user.update(user_params)
+          format.html { redirect_to @user, :notice => 'El usuario fue editado exitosamente' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.html { redirect_to @user, :alert => 'No tienes permisos para realizar esta acción' }
       end
     end
   end
 
   def destroy
-    @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, 
-        :alert => 'El usuario fue eliminado exitosamente' }
-      format.json { head :no_content }
+      if current_user.is_admin?
+        if @user.destroy
+          format.html { redirect_to users_url, 
+            :alert => 'El usuario fue eliminado exitosamente' }
+          format.json { head :no_content }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      else
+        format.html { redirect_to '/users', :alert => 'No tienes permisos para realizar esta acción' }
+      end
     end
   end
 
