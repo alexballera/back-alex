271cf25340d2a58afe511322af4d3525f17f50ec
alexballera <alexballera@gmail.com>
2015-10-01 20:58:03 -0430

    Configurado muchos a muchos articulos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -82,6 +82,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :description, :user_id, :image, :urlgit, :urlweb, :urlimg)
+      params.require(:article).permit(:title, :description, :user_id, :image, :urlgit, :urlweb, :urlimg, :category_ids => [])
     end
 end
---
+++ test/models/categories_project_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class CategoriesProjectTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
